fb133e159bf2394250973e997bbbb19351454444
Eyre Xi <109657355+Eyre-dutchess@users.noreply.github.com>
2025-06-22 06:00:54 +0800

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {    
+    experimental:{
+       
+        swcPlugins:[
+            ["next-superjson-plugin", {}]
+        ]
+    },
+    images:{
+        domains:[
+            "res.cloudinary.com",
+            "avatars.githubusercontent.com",
+            "lh3.googleusercontent.com"
+        ],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'res.cloudinary.com',
+          port: '**',}, 
+      ]
+    
+    }};
+
+export default nextConfig;
